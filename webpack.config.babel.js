c6595516f72e530453c31036f3707bdaba656e6e
khlevnov <olegkhlevnov@gmail.com>
2016-09-04 23:13:37 +0300

    Развернуто окружение для работы с проектом

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,21 @@
 'use strict';
 
+import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
+
 export default {
     context: __dirname + '/js',
     entry: './index.js',
     output: {
-        path: __dirname + '/build',
+        path: __dirname + '/public/js',
         filename: 'app.js'
-    }
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: {
+                baseDir: ['public']
+            }
+        })
+    ]
 }
