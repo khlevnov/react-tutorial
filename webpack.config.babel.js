86e70f2a147c956b51a644699dd5b30d18227192
khlevnov <olegkhlevnov@gmail.com>
2016-09-04 22:45:05 +0300

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default {
+    context: __dirname + '/js',
+    entry: './index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'app.js'
+    }
+}
